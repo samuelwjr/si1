5009217b779766543c7b73fd8d2d46e3fa84fa35
Samuel Wesley <samuelwjr@gmail.com>
2013-06-05 16:54:19 -0300

    projeto

---
+++ lab1/src/ufcg/si1/Classe.java
@@ -0,0 +1,13 @@
+package ufcg.si1;
+
+public class Classe {
+	
+	public static String xpto() {
+		return "Hello, World!";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(xpto());
+	}
+
+}
---
+++ lab1/src/ufcg/si1/TesteClasse.java
@@ -0,0 +1,14 @@
+package ufcg.si1;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TesteClasse {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello, World!", Classe.xpto());
+	}
+
+}
